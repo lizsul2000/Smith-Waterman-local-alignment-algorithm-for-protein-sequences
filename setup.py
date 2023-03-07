99415f134b56a5a6db7d523a839d617f83c03c34
Liu Gaoqianxue <45073440+lizsul2000@users.noreply.github.com>
2023-03-07 00:16:16 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='Smith-Waterman-local-alignment-algorithm-for-protein-sequences',
+    version='1.0.0',
+    license='GPL',
+    author="Gaoqianxue Liu",
+    author_email='qianxue.liu@yale.edu',
+    url='https://github.com/lizsul2000/Smith-Waterman-local-alignment-algorithm-for-protein-sequences',
+    keywords='Smith-Waterman',
+    install_requires=[
+          'argparse',
+      ],
